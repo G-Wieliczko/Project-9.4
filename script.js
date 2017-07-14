485290659e7e9bcec4a656753255925b00911e13
Grzegorz Wieliczko <gw851015@gmail.com>
2017-07-14 12:40:14 +0200

    added js script

---
+++ script.js
@@ -0,0 +1,13 @@
+var row = 4 ; 
+
+function drawTree(row) {
+	for (i = 0; i <= row; i++){
+var star = '';
+for (j = 0; j <= i; j++) { 
+	star += '*';
+	}
+	console.log(star)
+	}
+}
+
+drawTree(row);
